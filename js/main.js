9fe0cf3da730906bdf8091a9f60ba52728ab4d50
nikamgeliashvili <mgeliashvilia@gmail.com>
2021-05-01 00:20:34 +0400

    responsive

--- js/main.js
+++ js/main.js
@@ -80,8 +80,38 @@ $(document).ready(function(){
 	  $('.team_slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+
+  responsive: [
+	{
+	  breakpoint: 1024,
+	  settings: {
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		infinite: true,
+		dots: true
+	  }
+	},
+	{
+	  breakpoint: 992,
+	  settings: {
+		slidesToShow: 2,
+		slidesToScroll: 2
+	  }
+	},
+	{
+	  breakpoint: 768,
+	  settings: {
+		slidesToShow: 1,
+		slidesToScroll: 1
+	  }
+	}
+	
+  ]
+
 });
+
+
 
  	
 })
