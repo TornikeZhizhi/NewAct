081ba5148dafd642638953d700864a2f30680746
nikamgeliashvili <mgeliashvilia@gmail.com>
2021-06-20 02:12:08 +0400

    form

--- js/main.js
+++ js/main.js
@@ -210,6 +210,30 @@ $(document).ready(function(){
   });
 
 
+  $(document).on('keypress', ':input[type="number"]', function (e) {
+    if (isNaN(e.key)) {
+        return false;
+    }
+});
+
+
+$(document).on('change','.up', function(){
+	var names = [];
+	var length = $(this).get(0).files.length;
+	  for (var i = 0; i < $(this).get(0).files.length; ++i) {
+		  names.push($(this).get(0).files[i].name);
+	  }
+	  // $("input[name=file]").val(names);
+	if(length>2){
+	  var fileName = names.join(', ');
+	  $(this).closest('.form-group').find('.form-control').attr("value",length+" files selected");
+	}
+	else{
+	  $(this).closest('.form-group').find('.form-control').attr("value",names);
+	}
+ });
+
+
 
  
 
